5284f251b1e5361774622880aa3e2cbabd4a3902
Austinstevesk <austinstevesk@gmail.com>
2022-04-16 01:23:03 +0300

    Added valid soduko solution

---
+++ arrays_and_hashing/valid_soduko.py
@@ -0,0 +1,73 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. 
+Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+"""
+
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        
+        # we create hashsets for columns, rows and the squares; the squares are 3x3
+        cols = collections.defaultdict(set) # creates a hashset for each col position of each board Input
+        rows = collections.defaultdict(set) # creates a hashset for each row postion of  board Input
+        squares = collections.defaultdict(set) # keys = (col//3, row//3) -> craetes a hashset which stores the square each board input belongs to
+        
+        
+        for r in range(9): # 9 is the total number of rows
+            for c in range(9): # 9 is the total number of columns
+                # check whether the input of the board is blank and if it is, we continue since it is considered valid
+                if board[r][c] == ".": 
+                    continue
+                # check whether an input of the board exists in a certain row, a certain column or a certain square then return False if it does
+                if(board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                
+                # if the board input not in the above, update the values of the soduko
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+                
+        # return True if the above condition is not met
+        return True
+                          
+        
