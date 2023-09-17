2b719feab828f06c3c324b704c1f68dee70dbf52
Austinstevesk <austinstevesk@gmail.com>
2023-09-17 20:27:18 +0300

    add longest repeating character replacement

--- sliding-window/longest_repeating_character_replacement.py
+++ sliding-window/longest_repeating_character_replacement.py
@@ -5,23 +5,43 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # count = {} # hash map where keys are chars and values are char counts
+        # res = 0
+        # l = 0
+
+        # # right pointer is incrementing
+        # for r in range(len(s)):
+        #     count[s[r]] = 1 + count.get(s[r], 0)
+
+        #     # check whether the window is valid
+        #     # (right pointer - left pointer + 1) - maximum value of the chars) <= k
+        #     while (r - l + 1) - max(count.values()) > k:
+        #         # if not, shift the left pointer
+        #         count[s[l]] -= 1
+        #         l += 1
+        #     # result is contantly updated
+        #     res = max(res, r - l + 1)
+        # return res
+
+        ## better algorithm
         count = {} # hash map where keys are chars and values are char counts
         res = 0
         l = 0
+        max_freq = 0
 
         # right pointer is incrementing
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
+            max_freq = max(max_freq, count[s[r]])
 
-            # check whether the window is valid
-            # (right pointer - left pointer + 1) - maximum value of the chars) <= k
-            while (r - l + 1) - max(count.values()) > k:
+            while (r - l + 1) - max_freq > k:
                 # if not, shift the left pointer
                 count[s[l]] -= 1
                 l += 1
             # result is contantly updated
             res = max(res, r - l + 1)
         return res
+        
 
 
         """
